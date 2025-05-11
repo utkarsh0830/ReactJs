e1e7f0bbfc51a1a2c38736adee9f1976ef905a98
Utkarsh Agrawal <149224355+utkarsh0830@users.noreply.github.com>
2025-05-11 14:45:01 +0530

    Currency Converter Setup Completed

---
+++ currencyConverter/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './index.css';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1
+        className='text-3xl bg-orange-500'>Taiwind</h1>
+        
+    </>
+  )
+}
+
+export default App
---
+++ currencyConverter/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./index.html", // Include HTML files
+    "./src/**/*.{js,jsx,ts,tsx}" // Include all JS, JSX, TS, and TSX files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
